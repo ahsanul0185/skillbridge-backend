b6165abd25bf02c3dcc0f28aa19bd3730259a4ce
Ahsanul Haque <ahsanul0185@gmail.com>
2026-01-27 20:53:24 +0600

    installed better-auth

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
+import { toNodeHandler } from "better-auth/node";
 import express from "express";
+import { auth } from "./lib/auth";
 
 const app = express();
+
+app.all("/api/auth/*", toNodeHandler(auth));
+app.use(express.json());
 
 app.get("/", (_, res) => {
     res.json("Welcome to Skillbridge server")
