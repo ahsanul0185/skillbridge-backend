f4a1be5afb4cf6e92108bb300ef8a0b96179ee5b
Ahsanul Haque <ahsanul0185@gmail.com>
2026-01-28 15:39:50 +0600

    added filtering in get all tutors api

--- src/modules/tutor/tutor.controller.ts
+++ src/modules/tutor/tutor.controller.ts
@@ -1,13 +1,23 @@
 import type { NextFunction, Request, Response } from "express";
+import { tutorService } from "./tutor.service";
 
 const getAllTutors = async (req : Request, res : Response, next : NextFunction) => {
     try {
-       
+        
+        const filters = {
+            search : req.query.search ? req.query.search as string : null,
+            hourlyRate : req.query.hourlyRate ? Number(req.query.hourlyRate) as number : null,
+            categoryId : req.query.categoryId ? req.query.categoryId as string : null,
+            avgRating : req.query.avgRating ? Number(req.query.avgRating) as number : null,
+            totalReviews : req.query.totalReviews ? Number(req.query.totalReviews) as number : null
+        }
+
+       const result = await tutorService.getAllTutors(filters);
+       return res.status(200).json({success : true, message : "Tutors data retrieved successfully", data : result})
     } catch (e) {
        next(e)
     }
 }
-
 
 
 export const tutorController = {getAllTutors}
--- src/modules/tutor/tutor.service.ts
+++ src/modules/tutor/tutor.service.ts
@@ -1,7 +1,80 @@
+import type { TutorProfilesWhereInput } from "../../../generated/prisma/models";
+import { prisma } from "../../lib/prisma"
 
 
-const getAllTutors = async () => {
-    
+type FilterItems = {
+    search : string | null;
+    hourlyRate : number | null;
+    categoryId  : string | null;
+    avgRating : number | null;
+    totalReviews : number | null;
+}
+
+const getAllTutors = async ({search, hourlyRate, categoryId, avgRating, totalReviews} : FilterItems) => {
+//{search, hourlyRate, categoryId, avgRating, totalReviews}
+    const andConditions : TutorProfilesWhereInput[] = [];
+
+    if (search) {
+        andConditions.push({
+            OR : [
+                {
+                    user : {
+                        name : {
+                            contains : search,
+                            mode : "insensitive"
+                        }
+                    }     
+                },
+                {
+                    bio : {
+                        contains : search,
+                        mode : "insensitive"
+                    }
+                }
+            ],
+        })
+    }
+
+    if (hourlyRate) {
+        andConditions.push({
+            hourlyRate : {
+                lte : hourlyRate
+            }
+        })
+    }
+
+    if (categoryId) {
+        andConditions.push({
+            categoryId
+        })
+    }
+
+    if (avgRating) {
+        andConditions.push({
+            avgRating : {
+                gte : avgRating
+            }
+        })
+    }
+
+    if (totalReviews) {
+        andConditions.push({
+            totalReviews : {
+                gte : totalReviews
+            }
+        })
+    }
+
+
+    const result = await prisma.tutorProfiles.findMany({
+        where : {
+            AND : andConditions
+        },
+        include : {
+            user : true,
+        }
+    })
+    return result
 }
 
 
